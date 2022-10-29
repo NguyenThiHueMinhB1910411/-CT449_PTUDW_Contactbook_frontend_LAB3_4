4668eeaa640d4543c16e8f9c85aa657e998ee746
huongwisme <minhb1910411@student.ctu.edu.vn>
2022-10-29 15:41:42 +0700

    Khoi tao du an

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,44 @@
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: { ContactForm },
+
+    data() {
+        return {
+            contact: {
+                _id: "",
+                address: "",
+                email: "",
+                favorite: null,
+                name: "",
+                phone: "",
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                console.log(data);
+                await ContactService.create(data);
+                this.message = "Thêm liên hệ thành công !";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="page">
+        <h4 class="text-primary">Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page">
+        <p>
+            Oop, không thể tìm thấy trang chủ. Trở về
+            <route-link to="/">Trang chủ.</route-link>
+        </p>
+
+    </div>
+</template>
